2152b1781fdbf2d4a177a93b69754226f771d7df
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-03-24 05:57:05 +0000

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# installing flask using pip3 in puppet
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+  ensure    => '2.1.0',
+  provider  => 'pip3',
 }
+
