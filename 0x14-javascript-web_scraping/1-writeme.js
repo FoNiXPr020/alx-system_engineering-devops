f84bbe1ccea2143f2505a6a23d29439a56339165
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-04-23 23:12:23 +0100

    Update

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-try {
-  fs.writeFileSync(file, string, { encoding: 'utf8' });
-} catch (err) {
-  console.error(err);
-}
