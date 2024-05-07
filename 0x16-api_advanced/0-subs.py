b9a60f561e1b19ca90182448d6e4a4d2e1095573
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-05-07 16:45:46 +0100

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Retrieve the number of subscribers for a subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers={'User-Agent':
-                                          'os:root.alx-system_engineering-devops.0x16.api.advanced:v1.0'})
+                                          'os:api.advanced:v1.0'})
     if response.status_code != 200:
         return 0
     data = response.json()['data']
