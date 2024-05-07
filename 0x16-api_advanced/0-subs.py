b164e297b23ad95824a59ca67d667c25ede59381
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-05-07 16:39:36 +0100

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ headers = {"User-Agent": "Chrome/1.0/AppleWebKit/537.36"}
 def number_of_subscribers(subreddit):
     """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
