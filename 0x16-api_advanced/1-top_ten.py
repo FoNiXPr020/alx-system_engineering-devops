1c22c2dfa6bbbca70fbed0406fb8eded07123271
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-05-07 17:01:22 +0100

    Advanced API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,12 +4,15 @@ import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Ama45'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    data = response.json()['data']['children']
-    for post in data:
-        print(post['data']['title'])
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("None")
+    else:
+        posts = r.json()['data']['children']
+        for i in range(10):
+            print(posts[i]['data']['title'])
